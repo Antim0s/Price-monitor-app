d26d0df86953ac3831a23a3d2920dcdbaaa3f663
unknown <sieradzibartlomiej@gmail.com>
2026-06-15 10:50:21 +0200

    feat: dodanie systemu powiadomień desktopowych przy zmianie ceny (biblioteka plyer)

--- src/monitor.py
+++ src/monitor.py
@@ -1,5 +1,4 @@
-# src/monitor.py
-
+from plyer import notification  
 from src.scraper.core import PriceScraper
 from src.scraper.config import SHOPS_SELECTORS
 from src.database.db import DatabaseManager
@@ -19,7 +18,6 @@ class PriceMonitor:
 
         for product in products:
             product_id, name, url, shop_name = product
-            
             print(f"\nSprawdzam: {name} ({shop_name})")
             
             selector = SHOPS_SELECTORS.get(shop_name)
@@ -28,11 +26,47 @@ class PriceMonitor:
                 continue
 
             current_price = self.scraper.get_price(url, selector)
+            #current_price = 25.00
 
             if current_price:
+                latest_db_data = self.db.get_latest_price(product_id)
+                
+                if latest_db_data:
+                    old_price, _ = latest_db_data
+                    self._check_price_change(name, old_price, current_price)
+                else:
+                    print("-> To pierwsze sprawdzenie tego produktu. Brak historii do porównania.")
+
                 self.db.add_price_log(product_id, current_price)
                 print(f"-> SUKCES: Nowa cena {current_price} PLN zapisana w bazie.")
             else:
                 print("-> PORAŻKA: Nie udało się pobrać i zapisać ceny.")
                 
         print("\nAktualizacja cen zakończona!")
+
+    def _check_price_change(self, product_name: str, old_price: float, new_price: float):
+        if new_price < old_price:
+            roznica = round(old_price - new_price, 2)
+            title = "📉 Okazja! Cena spadła!"
+            message = f"Produkt '{product_name}' staniał o {roznica} PLN!\nStara cena: {old_price} PLN -> Nowa: {new_price} PLN"
+            self._send_notification(title, message)
+            
+        elif new_price > old_price:
+            roznica = round(new_price - old_price, 2)
+            title = "📈 Podwyżka ceny!"
+            message = f"Produkt '{product_name}' podrożał o {roznica} PLN.\nStara cena: {old_price} PLN -> Nowa: {new_price} PLN"
+            self._send_notification(title, message)
+        else:
+            print("-> Cena bez zmian.")
+
+    def _send_notification(self, title: str, message: str):
+        try:
+            notification.notify(
+                title=title,
+                message=message,
+                app_name="Price Tracker",
+                timeout=7  
+            )
+            print(f"-> Wysłano powiadomienie: {title}")
+        except Exception as e:
+            print(f"-> Błąd podczas wysyłania powiadomienia: {e}")
